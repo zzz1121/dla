208e2a6314c90706bf7a6fc7d8f0d46cc937fff0
mr_z <1034973568@qq.com>
2017-12-06 11:54:12 +0800

    s

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.94.8.230',
     // 数据库名
     'database'        => 'dla',
     // 用户名
